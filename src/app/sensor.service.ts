2141812edc023a4446c01b79aaf703bdd9f6a729
raifal <raifal@users.noreply.github.com>
2026-02-10 19:12:36 +0000

    added two more sensors

--- src/app/sensor.service.ts
+++ src/app/sensor.service.ts
@@ -15,7 +15,9 @@ export class SensorService {
       { id: 's1', name: 'Sensor Wohnzimmer', color: '#4dc9f6' },
       { id: 's2', name: 'Sensor Küche', color: '#f67019' },
       { id: 's3', name: 'Sensor Büro', color: '#f53794' },
-      { id: 's4', name: 'Sensor Aussen', color: '#537bc4' }
+      { id: 's4', name: 'Sensor Aussen', color: '#537bc4' },
+      { id: 's5', name: 'Sensor Boiler', color: '#000000' },
+      { id: 's6', name: 'Sensor Keller', color: '#166a8f' }
     ];
     return of(sensors);
   }
